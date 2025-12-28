497ab18551e38598403bca84c93bee2c8775e33e
vnt1c <ryanhung111@gmail.com>
2025-12-27 23:47:29 -0500

    finalized, created readme

--- include/sfmm.h
+++ include/sfmm.h
@@ -1,12 +1,3 @@
-/**
- * === DO NOT MODIFY THIS FILE ===
- * If you need some other prototypes or constants in a header, please put them
- * in another header file.
- *
- * When we grade, we will be replacing this file with our own copy.
- * You have been warned.
- * === DO NOT MODIFY THIS FILE ===
- */
 #ifndef SFMM_H
 #define SFMM_H
 #include <stdbool.h>
